08c18869175a14143a22c6a323a187aadfbed001
𝐒ᴀᴛʏᴜɢ ꭗ 𝐍ᴇᴛᴡᴏʀᴋ 🇮🇳™ <mitsurit42@gmail.com>
2026-05-13 17:05:58 +0530

    Add files via upload

---
+++ Lyra/utils/__init__.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------------------
+#  ShizuMusic © 2026
+#  Developed by Bad Munda ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
+
+from ShizuMusic.utils.permissions import is_user_authorized
+
+__all__ = ["is_user_authorized"]
