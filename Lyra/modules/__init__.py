0a4a0af0971e30fc873225a0dfd9ff81679201e4
𝐒ᴀᴛʏᴜɢ ꭗ 𝐍ᴇᴛᴡᴏʀᴋ 🇮🇳™ <mitsurit42@gmail.com>
2026-05-13 16:53:31 +0530

    Add files via upload

---
+++ Lyra/modules/__init__.py
@@ -0,0 +1,20 @@
+# --------------------------------------------------------------------------------
+#  ShizuMusic © 2026
+#  Developed by Bad Munda ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
+
+ALL_MODULES = [
+    "bot",
+    "play",
+    "pause",
+    "resume",
+    "skip",
+    "stop",
+    "ping",
+    "broadcast",
+    "callbacks",
+    "start",
+]
---
+++ Lyra/modules/pause.py
@@ -0,0 +1,55 @@
+# --------------------------------------------------------------------------------
+#  ShizuMusic © 2026
+#  Developed by Bad Munda ❤️
+#
+#  Unauthorized copying, editing, re-uploading or removing credits
+#  from this source code is strictly prohibited.
+# --------------------------------------------------------------------------------
+
+from pyrogram import filters
+from pyrogram.enums import ParseMode
+from pyrogram.types import Message
+
+from ShizuMusic import bot, call_py
+from ShizuMusic.utils.permissions import is_user_authorized
+
+
+# ─────────────────────────────────────────────
+# PAUSE STREAM
+# ─────────────────────────────────────────────
+@bot.on_message(filters.group & filters.command("pause"))
+async def pause_cmd(_, message: Message) -> None:
+
+    # Admin Check
+    if not await is_user_authorized(message):
+
+        await message.reply(
+            """
+<b>❍ ᴀᴅᴍɪɴ ᴏɴʟʏ</b>
+<b>❍ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ ɪs ғᴏʀ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴs.</b>
+""",
+            parse_mode=ParseMode.HTML,
+        )
+        return
+
+    # Pause Stream
+    try:
+
+        await call_py.pause(message.chat.id)
+
+        await message.reply(
+            """
+<b>❍ sᴛʀᴇᴀᴍ ᴘᴀᴜsᴇᴅ</b>
+<b>❍ ᴍᴜsɪᴄ ᴘʟᴀʏʙᴀᴄᴋ ᴛᴇᴍᴘᴏʀᴀʀɪʟʏ sᴛᴏᴘᴘᴇᴅ.</b>
+""",
+            parse_mode=ParseMode.HTML,
+        )
+
+    except Exception as e:
+
+        await message.reply(
+            f"""
+<b>❍ ᴘᴀᴜsᴇ ғᴀɪʟᴇᴅ</b><code>{e}</code>
+""",
+            parse_mode=ParseMode.HTML,
+        )
