2055326e2e952a107ab5e29972a991e99cef1259
𝐒ᴀᴛʏᴜɢ ꭗ 𝐍ᴇᴛᴡᴏʀᴋ 🇮🇳™ <mitsurit42@gmail.com>
2026-05-21 21:46:51 +0530

    LyraMusic 🕊️

--- Lyra/modules/stop.py
+++ Lyra/modules/stop.py
@@ -1,6 +1,6 @@
 # --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
+#  LyraMusic © 2026
+#  Developed by Team Satyug ❤️
 #
 #  Unauthorized copying, editing, re-uploading or removing credits
 #  from this source code is strictly prohibited.
@@ -10,10 +10,10 @@ from pyrogram import filters
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message
 
-from ShizuMusic import bot
-from ShizuMusic.core.call import leave_vc
-from ShizuMusic.core.queue import clear_queue, queue_size
-from ShizuMusic.utils.permissions import is_user_authorized
+from LyraMusic import bot
+from LyraMusic.core.call import leave_vc
+from LyraMusic.core.queue import clear_queue, queue_size
+from LyraMusic.utils.permissions import is_user_authorized
 
 
 # ─────────────────────────────────────────────
