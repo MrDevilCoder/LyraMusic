4de3303f4b93e7209f287ddf85a21f647a612d68
𝐒ᴀᴛʏᴜɢ ꭗ 𝐍ᴇᴛᴡᴏʀᴋ 🇮🇳™ <mitsurit42@gmail.com>
2026-08-05 11:09:06 +0530

    LyraMusic 🕊️
    Removed copyright notice and developer credits from __init__.py.

--- Lyra/modules/__init__.py
+++ Lyra/modules/__init__.py
@@ -1,11 +1,3 @@
-# --------------------------------------------------------------------------------
-#  LyrauMusic © 2026
-#  Developed by Team Satyug ❤️
-#
-#  Unauthorized copying, editing, re-uploading or removing credits
-#  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
-
 ALL_MODULES = [
     "bot",
     "play",
