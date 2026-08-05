e8477bc3bb84c22ed33e469e20e00eb75aa267c0
𝐒ᴀᴛʏᴜɢ ꭗ 𝐍ᴇᴛᴡᴏʀᴋ 🇮🇳™ <mitsurit42@gmail.com>
2026-08-05 13:55:11 +0530

    LyraMusic 🕊️

--- Lyra/modules/ping.py
+++ Lyra/modules/ping.py
@@ -1,11 +1,3 @@
-# --------------------------------------------------------------------------------
-#  LyraMusic © 2026
-#  Developed by Team Satyug ❤️
-#
-#  Unauthorized copying, editing, re-uploading or removing credits
-#  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
-
 import asyncio
 import os
 import time
