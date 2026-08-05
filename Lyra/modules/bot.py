0a9a2a39bd772eb6d28dabcec77463965fc06e76
𝐒ᴀᴛʏᴜɢ ꭗ 𝐍ᴇᴛᴡᴏʀᴋ 🇮🇳™ <mitsurit42@gmail.com>
2026-08-05 12:17:37 +0530

    Remove copyright notice from bot.py
    Removed copyright notice and developer information from the top of the bot.py file.

--- Lyra/modules/bot.py
+++ Lyra/modules/bot.py
@@ -1,11 +1,3 @@
-# --------------------------------------------------------------------------------
-#  LyraMusic © 2026
-#  Developed by Team Satyug ❤️
-#
-#  Unauthorized copying, editing, re-uploading or removing credits
-#  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
-
 import random
 
 from pyrogram import filters
