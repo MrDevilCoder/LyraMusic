ce56f8bb8d316761db49a007bc95d6065d3cb610
MrDevilCoder <mrdevilcoder@gmail.com>
2026-08-11 14:06:05 +0530

    Add files via upload

--- LyraMusic/utils/__init__.py
+++ LyraMusic/utils/__init__.py
@@ -1,11 +1,15 @@
 # --------------------------------------------------------------------------------
-#  LyraMusic © 2026
-#  Developed by Team Satyug ❤️
+#  ShizuMusic © 2026
+#  Developed by Bad Munda ❤️
 #
 #  Unauthorized copying, editing, re-uploading or removing credits
 #  from this source code is strictly prohibited.
 # --------------------------------------------------------------------------------
 
-from LyraMusic.utils.permissions import is_user_authorized
+from ShizuMusic.utils.permissions import is_user_authorized
+from ShizuMusic.utils.decorators import register_block_middleware
 
-__all__ = ["is_user_authorized"]
+__all__ = [
+    "is_user_authorized",
+    "register_block_middleware",
+]
