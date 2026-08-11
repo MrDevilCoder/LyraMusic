fb7dbc8ce5d1d6b0c549f34e09661ac37228832c
MrDevilCoder <mrdevilcoder@gmail.com>
2026-08-11 17:13:25 +0530

    LyraMusic 🕊️

--- LyraMusic/modules/stop.py
+++ LyraMusic/modules/stop.py
@@ -1,20 +1,12 @@
-# --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
-#
-#  Unauthorized copying, editing, re-uploading or removing credits
-#  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
-
 from pyrogram import filters
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message
 
-from ShizuMusic import bot
-from ShizuMusic.core.call import leave_vc
-from ShizuMusic.core.queue import clear_queue, queue_size
-from ShizuMusic.modules.block import group_allowed, user_allowed
-from ShizuMusic.utils.permissions import is_user_authorized
+from LyraMusic import bot
+from LyraMusic.core.call import leave_vc
+from LyraMusic.core.queue import clear_queue, queue_size
+from LyraMusic.modules.block import group_allowed, user_allowed
+from LyraMusic.utils.permissions import is_user_authorized
 
 
 # ── /stop & /end ──────────────────────────────────────────────────────────────
