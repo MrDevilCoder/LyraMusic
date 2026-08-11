0672f9fcbd098fc05454f221e9c4a8bc3ca445e3
MrDevilCoder <mrdevilcoder@gmail.com>
2026-08-11 14:07:19 +0530

    LyraMusic 🕊️

--- LyraMusic/utils/__init__.py
+++ LyraMusic/utils/__init__.py
@@ -1,13 +1,5 @@
-# --------------------------------------------------------------------------------
-#  ShizuMusic © 2026
-#  Developed by Bad Munda ❤️
-#
-#  Unauthorized copying, editing, re-uploading or removing credits
-#  from this source code is strictly prohibited.
-# --------------------------------------------------------------------------------
-
-from ShizuMusic.utils.permissions import is_user_authorized
-from ShizuMusic.utils.decorators import register_block_middleware
+from LyraMusic.utils.permissions import is_user_authorized
+from LyraMusic.utils.decorators import register_block_middleware
 
 __all__ = [
     "is_user_authorized",
